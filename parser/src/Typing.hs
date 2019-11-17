610eb031e686cb1bf252970efb07746ab9523e05
Yuuki Takano <ytakanoster@gmail.com>
2019-11-18 00:20:52 +0900

    implementing type checker for data structure

--- parser/src/Typing.hs
+++ parser/src/Typing.hs
@@ -3,6 +3,7 @@ module Typing where
 import qualified AST
 import qualified Control.Monad.State as S
 import qualified Data.HashMap        as MAP
+import qualified Data.HashSet        as SET
 import           Helper
 import           Module
 import qualified System.FilePath     as FP
@@ -16,6 +17,8 @@ data Named =
     deriving (Show)
 
 data LType = LType LModule (MAP.Map String Named) deriving (Show)
+
+type File2Mod = MAP.Map FP.FilePath (LModule, MAP.Map String Named)
 
 namedObj mod@(LModule file _ ast _) =
     (mod, S.evalState namedObjST (file, ast, MAP.empty))
@@ -63,3 +66,76 @@ addSumMem file dt (ast@(AST.SumMem pos id _):t) dict =
         let n = NamedSum dt ast
         let d = MAP.insert id n dict
         addSumMem file dt t d
+
+--isRecQTypeST :: a -> b -> [AST.QType] -> S.State (SET.Set (FP.FilePath, String)) Bool
+checkRecMem map mod (h:t) =
+    checkUserType h
+    where
+        checkUserType (AST.QType _ qual (AST.IDType pos id _)) =
+            case findUserObj map mod id of
+                Just x  -> case qual of
+                    Nothing -> pure True
+                    _       -> pure True
+                Nothing -> fail $ errMsg file pos "unkown type specifier"
+        file = mod2file mod
+
+checkRecSumMem map mod [] = pure True
+checkRecSumMem map mod (AST.SumMem _ _ tp:t) = do
+    checkRecMem map mod tp
+    checkRecSumMem map mod t
+
+checkRec :: File2Mod -> LModule -> (FP.FilePath, Named) -> S.State (SET.Set (FP.FilePath, String), FP.FilePath, AST.Position) Bool
+checkRec map mod (file, NamedData (AST.Data _ (AST.IDPos _ id) _ _ mem)) = do
+    (s, prevFile, prevPos) <- S.get
+    if SET.member (file, id) s then
+        fail $ errMsg prevFile prevPos "recursively defined"
+    else
+        checkRecSumMem map mod mem
+
+
+findUserObj :: File2Mod -> LModule -> [String] -> Maybe (FP.FilePath, Named)
+findUserObj map mod [id] =
+    case getObj objmap of
+        Just x  -> Just (file, x)
+        Nothing -> getObj2 imp
+    where
+        file = mod2file mod
+        imp = mod2imports mod
+        objmap = MAP.lookup file map
+        getObj (Just (_, m)) = MAP.lookup id m
+        getObj Nothing       = Nothing
+        getObj2 [] = Nothing
+        getObj2 ((AST.Import _ _ AST.ImportHere, imfile):t) =
+            case getObj (MAP.lookup imfile map) of
+                Just x  -> Just (imfile, x)
+                Nothing -> getObj2 t
+        getObj2 (_:t) =
+            getObj2 t
+findUserObj map mod id =
+    getObj modid imp
+    where
+        modid = modid2 $ reverse id
+        modid2 (h:t) = (reverse t, h)
+        file = mod2file mod
+        imp = mod2imports mod
+        getObj _ [] = Nothing
+        getObj x@(id, obj) ((AST.Import _ id' AST.ImportNS, imfile):t) =
+            if id == id' then
+                getObj2 obj (MAP.lookup imfile map) imfile
+            else
+                getObj x t
+        getObj x@(id, obj) ((AST.Import _ _ (AST.ImportAs id'), imfile):t) =
+            if id == id' then
+                getObj2 obj (MAP.lookup imfile map) imfile
+            else
+                getObj x t
+        getObj x (_:t) = getObj x t
+        getObj2 obj (Just (_, objmap)) imfile =
+            case MAP.lookup obj objmap of
+                Just x  -> Just (imfile, x)
+                Nothing -> Nothing
+        getObj2 _ _ _ = Nothing
+
+
+mod2file (LModule f _ _ _) = f
+mod2imports (LModule _ _ _ i) = i
